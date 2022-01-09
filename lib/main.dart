076a4f7950966310d842b6e3da1d54ae69b7869c
Glico <dltkdgus0226@naver.com>
2022-01-09 23:31:37 +0900

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final dummyItems = [
+  'Mitrox_logo.png',
+  'Mitrox_logo.png',
+  'Mitrox_logo.png',
+];
 
 void main() {
   runApp(MyApp());
@@ -24,13 +32,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var _index = 0;
+  var _pages = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text(
-          '복잡한 위젯',
+          'Mitrox',
           style: TextStyle(color: Colors.black),
         ),
         centerTitle: true,
@@ -44,12 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          '$_index 페이지',
-          style: TextStyle(fontSize: 40),
-        ),
-      ),
+      body: _pages[_index],
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
           setState(() {
@@ -71,6 +80,174 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: Icon(Icons.account_circle),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  final url = 'https://opensea.io/Mitrox_Token_NFT';
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
+    );
+  }
+
+  Widget _buildTop() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: () async {
+                  await launch(url, forceWebView: true, forceSafariVC: true);
+                },
+                child: Column(children: <Widget>[
+                  Icon(
+                    Icons.menu,
+                    size: 40,
+                  ),
+                  Text('Menu 1'),
+                ]),
+              ),
+              Column(children: <Widget>[
+                Icon(
+                  Icons.menu,
+                  size: 40,
+                ),
+                Text('Menu 2'),
+              ]),
+              Column(children: <Widget>[
+                Icon(
+                  Icons.menu,
+                  size: 40,
+                ),
+                Text('Menu 3'),
+              ]),
+              Column(children: <Widget>[
+                Icon(
+                  Icons.menu,
+                  size: 40,
+                ),
+                Text('Menu 4'),
+              ]),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(children: <Widget>[
+                Icon(
+                  Icons.menu,
+                  size: 40,
+                ),
+                Text('Menu 5'),
+              ]),
+              Column(children: <Widget>[
+                Icon(
+                  Icons.menu,
+                  size: 40,
+                ),
+                Text('Menu 6'),
+              ]),
+              Column(children: <Widget>[
+                Image.asset(
+                  'Mitrox_logo.png',
+                  height: 50,
+                  width: 50,
+                ),
+                Text('Menu 7'),
+              ]),
+              Opacity(
+                opacity: 0.0,
+                child: Column(children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('Menu 8'),
+                ]),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMiddle() {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 300.0,
+        autoPlay: true,
+      ),
+      items: dummyItems.map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(color: Colors.amber),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.asset(
+                  i,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildBottom() {
+    final items = List.generate(10, (i) {
+      return ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[이벤트] 이것은 공지사항입니다.'),
+      );
+    });
+
+    return ListView(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '이용서비스',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '내 정보',
+        style: TextStyle(fontSize: 40),
       ),
     );
   }
